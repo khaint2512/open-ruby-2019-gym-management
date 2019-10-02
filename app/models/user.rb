60027c04e5b5bffa4a3be51194085ed03d7b7751
khaint2512 <trongkhai2598@gmail.com>
2019-10-03 00:47:26 +0700

    Signup for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,4 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: Settings.user.email.max_length }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
   has_secure_password
-
-
 end
